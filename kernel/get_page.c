98127e9186716d9926e71fef3f00ad2c60122422
majosue <majosue@debian>
2023-05-04 10:10:44 -0400

    get page table entry

--- kernel/get_page.c
+++ kernel/get_page.c
@@ -48,8 +48,23 @@ static void free_page_in_lowmem(void *page, uint32_t nbr)
 	}
 }
 
-int is_page_maped(uint32_t ptr) // вынести получение привязаной страницы в общую функию?
+/* https://wiki.osdev.org/Paging */
+uint32_t *get_page_table_entry(void *virtualaddr) 
+{  
+	uint32_t pdindex = (uint32_t)virtualaddr >> 22;
+    uint32_t ptindex = (uint32_t)virtualaddr >> 12 & 0x03FF;
+    // Here you need to check whether the PD entry is present.
+    uint32_t *pt = ((uint32_t *)0xFFC00000) + (0x400 * pdindex);
+    // Here you need to check whether the PT entry is present.
+	uint32_t *pt_entry = &pt[ptindex];
+ 
+    return (uint32_t *)((uint32_t)pt_entry & ~0xFFF);
+}
+
+int is_page_maped(void *ptr) // вынести получение привязаной страницы в общую функию?
 {
+	return (*get_page_table_entry(ptr) & 1);
+	/* 
 	uint32_t *pde = (uint32_t *) 0xfffff000;
 	uint32_t pde_index = ptr >> 22;
 	uint32_t table_index = (ptr & 0x3FF000) >> 12;
@@ -61,13 +76,14 @@ int is_page_maped(uint32_t ptr) // вынести получение привя�
 
 	printf("%x\n", table[table_index] & 1);
 	return (table[table_index] & 1);
-}
+	*/
+} 
 
 static void *__get_virt_page(void **start, void* end, uint32_t nbr)
 {
 	uint32_t i = 0;
 	void *result = *start;
-	while (*start < end && !is_page_maped((uint32_t)*start)) {
+	while (*start < end && !is_page_maped(*start)) {
 		if (i >= nbr) {
 			return (result);
 		}
@@ -94,22 +110,10 @@ uint32_t *get_non_continuous_frames(uint32_t nbr)
 	return (frames);
 }
 
-uint32_t *get_table_entry(void *page)
-{
-	uint32_t *pde = (uint32_t *) 0xfffff000;
-	uint32_t pde_index = (uint32_t)page >> 22;
-	uint32_t table_index = ((uint32_t)page & 0x3FF000) >> 12;
-	uint32_t *table = (uint32_t *)((pde[pde_index] & 0xFFFFF000) + PAGE_OFFSET);
-
-	printf("page %x table %x table[%u] %x\n", page, table, table_index, table[table_index]);
-
-	return (&table[table_index]);
-}
-
 static void map_pages(void *pages, uint32_t *frames, uint32_t nbr)
 {
 	for (uint32_t i = 0; i < nbr; i++, pages += PAGE_SIZE) {
-		*get_table_entry(pages) = frames[i] | 3;
+		*get_page_table_entry(pages) = frames[i] | 3;
 	}
 	refresh_map();
 }
@@ -131,10 +135,10 @@ static void *get_page_in_highmem(uint32_t nbr) // делаем так чтоб �
 
 	printf("Start = %#x\n", start);
 	printf("End = %#x\n", end);
-	printf("is maped start, %d\n", is_page_maped((uint32_t)start));
+	printf("is maped start, %d\n", is_page_maped(start));
 
 	while (start < end && pages == NULL) {
-		if (!is_page_maped((uint32_t)start)) {
+		if (!is_page_maped(start)) {
 			pages = __get_virt_page(&start, end, nbr);
 		}
 		start += PAGE_SIZE;
